55526bdcdce7b319ab6e28ad9117b7ea76ed5072
Mats Rauhala <mats.rauhala@gmail.com>
2011-07-09 19:53:15 +0300

    Update to hakyll 3.2

--- site.hs
+++ site.hs
@@ -6,6 +6,7 @@ import Prelude hiding (id)
 import Control.Category (id)
 import Control.Arrow ((***), (>>>), arr)
 import Data.Monoid
+import Data.List
 import Hakyll.Main
 import Hakyll.Core.Routes
 import Hakyll.Core.Util.Arrow
@@ -16,53 +17,61 @@ import Hakyll.Web.CompressCss
 import Hakyll.Web.Template
 import Hakyll.Web.RelativizeUrls
 import Hakyll.Web.Page
+import Hakyll.Web.Page.List
 import Hakyll.Web.Page.Metadata
+
 
 main = hakyll $ do
   -- CSS
-  route "css/*" idRoute
-  compile "css/*" compressCssCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
   -- JS
-  route "static/js/*" idRoute
-  compile "static/js/*" copyFileCompiler
+  match "static/js/*" $ do
+    route idRoute
+    compile copyFileCompiler
   -- JS-lang
-  route "static/js/lang/*" idRoute
-  compile "static/js/lang/*" copyFileCompiler
+  match "static/js/lang/*" $ do
+    route idRoute
+    compile copyFileCompiler
   -- Images
-  route "static/img/*" idRoute
-  compile "static/img/*" copyFileCompiler
+  match "static/img/*" $ do
+    route idRoute
+    compile copyFileCompiler
   -- About
-  route "about.markdown" $ setExtension ".html"
-  compile "about.markdown" $ pageCompiler
-    >>> applyTemplateCompiler "templates/default.html"
-    >>> relativizeUrlsCompiler
+  match "about.markdown" $ do
+    route $ setExtension "html"
+    compile $ pageCompiler
+      >>> applyTemplateCompiler "templates/default.html"
+      >>> relativizeUrlsCompiler
   -- Posts
-  route "posts/*" $ setExtension ".html"
-  compile "posts/*" $ pageCompiler
-    >>> arr (renderDateField "date" "%B %e, %Y" "Date unknown")
-    >>> applyTemplateCompiler "templates/post.html"
-    >>> applyTemplateCompiler "templates/default.html"
-    >>> relativizeUrlsCompiler
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pageCompiler
+      >>> arr (renderDateField "date" "%B %e, %Y" "Date unknown")
+      >>> applyTemplateCompiler "templates/post.html"
+      >>> applyTemplateCompiler "templates/default.html"
+      >>> relativizeUrlsCompiler
   -- Index
-  route "index.html" idRoute
+  match "index.html" $ route idRoute
   create "index.html" $ constA mempty
     >>> arr (setField "title" "Home")
-    >>> requireAllA "posts/*" (id *** arr (take 3 . reverse . sortByBaseName) >>> addPostList)
+    >>> requireAllA "posts/*" (id *** arr (take 3 . recentFirst) >>> addPostList)
     >>> applyTemplateCompiler "templates/index.html"
     >>> applyTemplateCompiler "templates/default.html"
     >>> relativizeUrlsCompiler
   -- Posts list
-  route "posts.html" idRoute
+  match "posts.html" $ route idRoute
   create "posts.html" $ constA mempty
     >>> arr (setField "title" "Posts")
     >>> requireAllA "posts/*" addPostList
     >>> applyTemplateCompiler "templates/index.html"
     >>> applyTemplateCompiler "templates/default.html"
     >>> relativizeUrlsCompiler
-  compile "templates/*" templateCompiler
+  match "templates/*" $ compile templateCompiler
   where
     addPostList = setFieldA "posts" $
-      arr (reverse . sortByBaseName)
-	>>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
-	>>> arr mconcat
-	>>> arr pageBody
+      arr recentFirst
+        >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
+        >>> arr mconcat
+        >>> arr pageBody
